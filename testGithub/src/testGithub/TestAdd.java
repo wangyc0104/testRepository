f7ffde6006877d33cb5e9c9f8dc30cb08864d2d3
wangyc0104 <wangyc0104@126.com>
2019-05-17 15:47:04 +0800

    modify TestAdd.java

--- testGithub/src/testGithub/TestAdd.java
+++ testGithub/src/testGithub/TestAdd.java
@@ -2,6 +2,6 @@ package testGithub;
 
 public class TestAdd {
 	public static void main(String[] args) {
-		System.out.println("Add another .java file");
+		System.out.println("Add another java file");
 	}
 }
