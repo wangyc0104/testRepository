949a0eb948710d06b07d995d538947e1d6641e2b
wangyc0104 <wangyc0104@126.com>
2019-05-17 20:13:07 +0800

    Add a line in TestAdd.java

--- testGithub/src/testGithub/TestAdd.java
+++ testGithub/src/testGithub/TestAdd.java
@@ -2,6 +2,7 @@ package testGithub;
 
 public class TestAdd {
 	public static void main(String[] args) {
-		System.out.println("Add another java file");
+		System.out.println("Add TestAdd.java");
+		System.out.println("Add a line in TestAdd.java");
 	}
 }
